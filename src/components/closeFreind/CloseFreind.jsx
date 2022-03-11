96501defab465101a79bae966a589f752d3bd1e4
software-eng1neer <otabekoktamov004@gmail.com>
2022-03-11 11:47:54 +0500

    commit

--- src/components/closeFreind/CloseFreind.jsx
+++ src/components/closeFreind/CloseFreind.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import "./closeFreind.css"
+import React from "react";
+import "./closeFreind.css";
 
-function CloseFreind({user}) {
-    return (
-        <li className="sidebarFreind">
-                        <img className="sidebarFreindImg" src={user.profilePicture}/>
-                        <span className="sidebarFreindName">{user.username}</span>
-                    </li>
-    )
+function CloseFreind({ user }) {
+  return (
+    <li className="sidebarFreind">
+      <img alt="rasm" className="sidebarFreindImg" src={user.profilePicture} />
+      <span className="sidebarFreindName">{user.username}</span>
+    </li>
+  );
 }
 
-export default CloseFreind
+export default CloseFreind;
--- src/components/online/Online.jsx
+++ src/components/online/Online.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import "./online.css"
+import React from "react";
+import "./online.css";
 
-function Online({user}) {
-    return (
-        <li className="rightbarFreind">
-        <div className="rightbarProfileImgContainer">
-            <img className="rightbarProfileImg" src={user.profilePicture}/>
-            <span className="rightbarOnline"></span>
-        </div>
-        <span className="rightbarUsername">{user.username}</span>
+function Online({ user }) {
+  return (
+    <li className="rightbarFreind">
+      <div className="rightbarProfileImgContainer">
+        <img
+          alt="img"
+          className="rightbarProfileImg"
+          src={user.profilePicture}
+        />
+        <span className="rightbarOnline"></span>
+      </div>
+      <span className="rightbarUsername">{user.username}</span>
     </li>
-    )
+  );
 }
 
-export default Online
+export default Online;
